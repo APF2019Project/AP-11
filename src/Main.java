7d70c3582069e09c1987f86ef60f7a677fad6ba0
kian <bakhtari.kian@gmail.com>
2019-12-18 21:27:10 +0330

    check

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("conflict bokhor");
+
         Menu.loginMenu();
-        int a = 20;
-        System.out.println("wtf");
+
     }
 }
