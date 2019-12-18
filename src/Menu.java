3b52ac92ad0f95f848d47588c43225ed9cb7afca
kian <bakhtari.kian@gmail.com>
2019-12-18 20:59:52 +0330

    ready for your push

--- src/Menu.java
+++ src/Menu.java
@@ -31,7 +31,7 @@ public class Menu {
 
     static void mainMenu() {
 
-        
+
 
     }
 }
--- src/PlayGround.java
+++ src/PlayGround.java
@@ -1,16 +1,3 @@
-import com.sun.jdi.ArrayReference;
-
-import java.util.ArrayList;
-
 public class PlayGround {
-
-    private static Unit[][] playGround = new Unit[19][6];
-
-    private static ArrayList<Plant> allPlants = new ArrayList<>();
-    private static ArrayList<Zombie> allZombies = new ArrayList<>();
-    private static ArrayList<Shoot> allShoots = new ArrayList<>();
-
-
-
 
 }
