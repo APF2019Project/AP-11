a48b229cd4fd5ec13d740bdd39cfd002c9d87102
kian <bakhtari.kian@gmail.com>
2019-12-18 21:15:16 +0330

    pooya

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("conflict bokhor");
         Menu.loginMenu();
+        int a = 20;
+        System.out.println("wtf");
     }
 }
