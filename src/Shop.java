1f8b20c8b8ef53c89986260460b7cd76f04c52e6
Mohamad9731 <mohamad0namdar@gmail.com>
2019-12-22 22:59:34 +0330

    prices were added and Shop changed relatively

--- src/Shop.java
+++ src/Shop.java
@@ -5,13 +5,21 @@ public class Shop {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void shop() {
-        System.out.println("***Shop***\nEnter command:");
         String command;
-        while (true) {
-         command = scanner.nextLine();
+        boolean exitShop = false;
+        while (!exitShop) {
+            System.out.println("***Shop***\nEnter command:");
+            command = scanner.nextLine();
+            if (command.matches("[b,B]uy (.+)")) {
+                command = command.replaceFirst("[b,B]uy", "");
+                command = command.trim();
+                buy(Account.getPlayingAccount(), command);
+                continue;
+            }
             switch (command) {
                 case "Exit":
                 case "exit":
+                    exitShop = true;
                     break;
                 case "Show shop":
                 case "show shop":
@@ -25,52 +33,75 @@ public class Shop {
                 case "money":
                     showMoney(Account.getPlayingAccount());
                 default:
-                    if (command.matches("[b,B]uy (.+)")) {
-                        command = command.replaceFirst("[b,B]uy", "");
-                        command = command.trim();
-                        buy(Account.getPlayingAccount(), command);
-                    } else {
-                        System.out.println("invalid command in Login Menu\nTry again:");
-                    }
+                    System.out.println("invalid command in Login Menu\nTry again:");
             }
         }
     }
 
     public static void showShop(Account account) {
-        ArrayList<Plant> collection = account.getPlantsCollection();
-        for (Plant plant : Plant.getPlants()) {
-            boolean cardIsSold = false;
-            for (int i =0; i < collection.size(); i++)
-                if (plant == collection.get(i)) {
-                    cardIsSold = true;
+        Plant[] plantsCollection = (Plant[]) Plant.getPlants().toArray();
+        Zombie[] zombiesCollection = (Zombie[]) Zombie.getZombies().toArray();
+        int numOfPlants = plantsCollection.length;
+        int numOfZombies = zombiesCollection.length;
+        for (int i = 0; i < numOfPlants; i++)
+            for (Plant plant : account.getPlantsCollection())
+                if (plantsCollection[i] == plant) {
+                    plantsCollection[i] = null;
                     break;
                 }
-            if (cardIsSold)
-                continue;
-            System.out.println(plant.getName() +": "+plant.getSunCost());
-        }
+        for (int i = 0; i < numOfZombies; i++)
+            for (Zombie zombie : account.getZombiesCollection())
+                if (zombiesCollection[i] == zombie) {
+                    zombiesCollection[i] = null;
+                    break;
+                }
+        // still needs work //
+
+        /*int i = 0;
+        int j = 0;
+
+        while (i < numOfPlants || j < numOfZombies) {
+            if (i < numOfPlants) {
+                System.out.print();
+            }
+            i++;
+        }*/
     }
 
     public static void buy(Account account, String cardName) {
-        for (Plant plant: Plant.getPlants())
-            if (cardName.equals(plant.getName())){
-                account.getPlantsCollection().add(plant);
-                return;
-            }
+        for (Plant plant : Plant.getPlants())
+            if (cardName.equals(plant.getName()))
+                if (account.getMoney() >= plant.getPrice()) {
+                    account.setMoney(account.getMoney() - plant.getPrice());
+                    account.getPlantsCollection().add(plant);
+                    return;
+                }
+        for (Zombie zombie : account.getZombiesCollection())
+            if (cardName.equals(zombie.getName()))
+                if (account.getMoney() >= zombie.getPrice()) {
+                    account.setMoney(account.getMoney() - zombie.getPrice());
+                    account.getZombiesCollection().add(zombie);
+                    return;
+                }
     }
 
-    public static void showMoney(Account account){
-        System.out.println("Your money: "+ account.getMoney());
+    public static void showMoney(Account account) {
+        System.out.println("Your money: " + account.getMoney());
     }
 
     public static void showCollection(Account account) {
-        Plant[] collection = (Plant[]) account.getPlantsCollection().toArray();
-        for (int i = 0; i<collection.length; i++){
-            if (i +1 == collection.length){
-                System.out.println(collection[i].getName());
-                break;
-            }
-            System.out.print(collection[i].getName()+" ,\t");
+        System.out.println(" Plants\tZombies ");
+        Plant[] plants = (Plant[]) account.getPlantsCollection().toArray();
+        Zombie[] zombies = (Zombie[]) account.getZombiesCollection().toArray();
+        int i = 0;
+        while (i < plants.length || i < zombies.length) {
+            if (i < plants.length && i < zombies.length)
+                System.out.println(plants[i].getName() + "\t" + zombies[i].getName());
+            else if (i < plants.length)
+                System.out.println(plants[i].getName());
+            else
+                System.out.println(" \t\t " + zombies[i].getName());
+            i++;
         }
     }
 }
