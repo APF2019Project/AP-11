9797e553ac209476eef020c2484e39557f076059
kian <bakhtari.kian@gmail.com>
2019-12-31 14:12:55 +0330

    railing

--- src/Rail.java
+++ src/Rail.java
@@ -2,9 +2,39 @@ import java.util.ArrayList;
 
 public class Rail extends Play {
 
-    public static ArrayList<Plant> railPlants = new ArrayList<>();
+    public static ArrayList<Plant> railDeck = new ArrayList<>();
+    private static int turn = 0;
+    private static boolean addPlantInNextTurn = false;
+    private static int lastAddedTurn = 1000;
+
+
+    public static Plant generateRandomPlant() {
+        Plant plant = null;
+        int ranNum = (int) (Math.random() * 24);
+        int i = 0;
+        for (Plant plantIterator : Plant.getPlants()) {
+            if (i < ranNum || plantIterator.isCanBePlantedInWater()) {
+                i++;
+                continue;
+            }
+
+            plant = new Plant(plantIterator);
+            break;
+        }
+        return plant;
+    }
+
+    public static void addRandomPlantToRailDeck() {
+
+        if (railDeck.size() < 10) {
+            railDeck.add(generateRandomPlant());
+        }
+    }
+
+    private static void generate()
 
     public static void railTurn() {
+
 
 
         Menu.railMenu();
