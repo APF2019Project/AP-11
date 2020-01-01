4c7c465f5fa1fa487764e85a50561b92602927b4
Sajad Faghfoor Maghrebi <sajad.faghfoor@gmail.com>
2020-01-01 15:03:15 +0330

    may it be pvp don't be that hard.

--- src/PvP.java
+++ src/PvP.java
@@ -1,28 +1,57 @@
 public class PvP {
 
+    static int waveNumbers;
+    static boolean IsPvPTrue;
+    static Account account1;
+    static Account account2;
+
     public static void pvpTurn() {
-        Account account1 = Account.getPlayingAccount(); // It is for plantPlayingAccount
-        Account account2 = Account.getZombiePlayingAccount();
-
-        boolean IsPvPTrue = true;
-
+        account1 = Account.getPlayingAccount(); // It is for plantPlayingAccount
+        account2 = Account.getZombiePlayingAccount();
+        IsPvPTrue = true;
+        setWaveNumber(3); // could be any number
         while (IsPvPTrue) {
-
             if (checkFinished()) {
-                doFinalThings();
+                doFinalThings("Zombie");
                 return;
             }
             Shoot.shootTurn();
             // shoot turn is done
 
+            if (checkFinished()) {
+                doFinalThings("Plant");
+                return;
+            }
 
+            Plant.plantsTurn();
+            Menu.dayMenu(1); // probably need playingAccount
+            // plant is done
 
+            if (checkFinished()) {
+                doFinalThings("Plant");
+                return;
+            }
+
+            Zombie.zombiesTurn();
+            Menu.zombieMenu();
+            // zombie is done
         }
 
         Menu.pvpMenu();
     }
 
-    private static void doFinalThings() {
+    private static void setWaveNumber(int waveNum) {
+        waveNumbers = waveNum;
+    }
+
+    private static void doFinalThings(String whoWon) {
+        if (IsPvPTrue) {
+            if (waveNumbers > 0) {
+                waveNumbers--;
+            } else {
+                IsPvPTrue = false;
+            }
+        }
     }
 
     private static boolean checkFinished() {
